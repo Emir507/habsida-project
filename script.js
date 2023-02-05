b672b46250f23248189e98200282359894f67708
emir <admin@Mac-Mini.local>
2023-02-05 13:38:01 +0600

    sidebar added

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-const burgerMenuOpen = document.querySelector('.burger-menu')
-const burgerMenuClose = document.querySelector('.sidebar--close')
-const sidebar = document.querySelector('.sidebar')
+const burgerMenuOpen = document.querySelector(".burger-menu");
+const burgerMenuClose = document.querySelector(".close-menu-btn");
+const sidebar = document.querySelector(".sidebar");
+const body = document.body;
 
+function toggleSidebar() {
+  sidebar.classList.toggle("sidebar--visible");
+  toggleOverlay();
+}
+function toggleOverlay() {
+  document.body.classList.toggle("overlay");
+}
+function hideSidebarAndOverlayOnVoidClick(e) {
+  if (e.target.classList.contains("overlay")) {
+    toggleSidebar();
+  }
+}
+function toggleOnOverlayClickEvent() {
+  if (sidebar.classList.contains("sidebar--visible")) {
+    document.body.addEventListener(
+      "click",
+      hideSidebarAndOverlayOnVoidClick,
+      true
+    );
+  } else {
+    document.body.removeEventListener(
+      "click",
+      hideSidebarAndOverlayOnVoidClick,
+      true
+    );
+  }
+}
 
-burgerMenuOpen.addEventListener('click', () => {
-  sidebar.classList.remove('sidebar-slide-out')
-  sidebar.classList.add('sidebar-slide-in')
-})
-burgerMenuClose.addEventListener('click', () => {
-  sidebar.classList.remove('sidebar-slide-in')
-  sidebar.classList.add('sidebar-slide-out')
-})
+burgerMenuOpen.addEventListener("click", () => {
+  toggleSidebar();
+  toggleOnOverlayClickEvent();
+});
+
+burgerMenuClose.addEventListener("click", () => {
+  toggleSidebar();
+  toggleOnOverlayClickEvent();
+});
